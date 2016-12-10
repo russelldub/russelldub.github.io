59f39f2b3480c36ecf5b15f252ef56f265880fe0
Russell Whitesides <russelldub@gmail.com>
2016-12-09 21:11:58 -0800

    Add fullscreen controls lib
    * JS+Font Awesome icons
    * Add fullscreen to symm sketcher grid

---
+++ scripts/local/fs.js
@@ -0,0 +1,61 @@
+var fs = {
+  fs_elements: {},
+
+  //launchIntoFullscreen(p5div);
+  //https://davidwalsh.name/fullscreen
+  launchIntoFullscreen: function(element) {
+    if(element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if(element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if(element.mozRequestFullScreen) {
+         element.mozRequestFullScreen();
+    } else if(element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+  },
+
+  addFullScreenControl: function(element, width, height, callback)  {
+    fs.fs_elements[element] = {toggle: false, width: width, height: height, callback: callback}
+
+    var fs_icon_elem = document.createElement("i");
+    fs_icon_elem.setAttribute("class", "fa fa-arrows-alt");
+    fs_icon_elem.onclick = function() {fs.fs_elements[element].toggle=true; fs.launchIntoFullscreen(element)};
+//    fs_icon_elem.style.color = "white";
+    fs_icon_elem.style.position = "relative";
+    fs_icon_elem.style.float = "right";
+//    fs_icon_elem.style.top = "-24px";
+//    fs_icon_elem.style.zIndex = "-1";
+    element.appendChild(fs_icon_elem);
+    element.style.width = width+"px"
+    element.style.height = height+"px"
+
+    //http://stackoverflow.com/questions/8796988/binding-multiple-events-to-a-listener-without-jquery
+    function addListenerMulti(el, s, fn) {
+      var evts = s.split(' ');
+      for (var i=0, iLen=evts.length; i<iLen; i++) {
+        el.addEventListener(evts[i], fn, false);
+      }
+    };
+
+    addListenerMulti(document,'webkitfullscreenchange mozfullscreenchange fullscreenchange', function(event) {
+        console.log("even fired!");	
+        element_keys=Object.keys(fs.fs_elements);
+	for(var i = 0 ; i < element_keys.length; ++i) {
+          if(fs.fs_elements[element_keys[i]].toggle) {
+	      console.log("go big.");
+              fs.fs_elements[element_keys[i]].callback(screen.width,screen.height);
+              fs.fs_elements[element_keys[i]].toggle = false;
+          } else { 
+	      console.log("go home.");
+	      console.log(fs.fs_elements);
+              var width = fs.fs_elements[element_keys[i]].width;
+              var height = fs.fs_elements[element_keys[i]].height;
+	      console.log("%d, %d",width,height);
+	      //resizeCanvas(width,height);
+              fs.fs_elements[element_keys[i]].callback(width,height);
+          }
+        }
+    } );
+  }
+};
